fab0e9b415f435f146d28f12c10faa3fefe3717e
Sadik Saifi <sadiksaifi205@gmail.com>
2022-10-21 15:49:08 +0530

    added my neovim config

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,23 @@
+local colorscheme = "gruvbox"
+
+-- For gruvbox-material
+vim.cmd([[
+  let g:gruvbox_material_background = 'hard'
+  let g:gruvbox_material_better_performance = 1
+  let g:gruvbox_material_transparent_background = 1
+  let g:gruvbox_material_ui_contrast = 'high'
+]])
+
+-- For gruvbox
+vim.cmd([[
+    let g:gruvbox_contrast_dark = 'hard'
+    let g:gruvbox_transparent_bg = 1
+    autocmd SourcePost * highlight Normal ctermbg=NONE guibg=NONE
+    highlight LineNr ctermbg=NONE guibg=NONE
+    highlight SignColumn ctermbg=NONE guibg=NONE
+]])
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+	return
+end
